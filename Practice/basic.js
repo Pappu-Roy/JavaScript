076c2b8ede448aa550567087f2ebec3738694fd6
Pappu <pappuroy10102000@gmail.com>
2025-07-16 00:56:20 +0600

    basics

---
+++ Practice/basic.js
@@ -0,0 +1,9 @@
+console.log("Hello World");     
+console.log(2000+25);
+let leo="Messi";
+console.log(`I love ${leo}. ${leo} is the GOAT of Football.`);  // Printing with back => ` `
+console.log('I love '+leo+'.'+'I know '+leo+' is a GOAT.');     // General printing
+
+var a = 20;
+var b = 50;
+console.log(a+b);
