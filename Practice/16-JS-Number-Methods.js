5dd0dfa4cdb87739337f6da498f90fc26d08366e
Pappu <pappuroy10102000@gmail.com>
2025-07-24 12:29:00 +0600

    JS Number Methods

---
+++ Practice/16-JS-Number-Methods.js
@@ -0,0 +1,81 @@
+//! Pappu Roy
+//! CSE SUST
+
+// JavaScript Number Methods 
+let x = 123;
+console.log(x);
+x = x.toString();
+console.log(typeof x); // Convert Number to String 
+console.log((123).toString());
+
+// Number to String Conversion
+console.log((100 + 23).toString()); // Convert to String
+console.log((100 + 23).toString(2)); // Convert to Binary
+console.log((100 + 23).toString(8)); // Convert to Octal
+console.log((100 + 23).toString(16)); // Convert to Hexadecimal
+
+// Number to Exponential Notation
+console.log("Exponential Notation:");
+let y = 4;
+console.log(y.toExponential(2)); // Convert to Exponential Notation
+console.log(y.toExponential(4)); // More precision in Exponential Notation
+console.log(y.toExponential(6)); // Even more precision in Exponential Notation
+
+// tofixed() Method
+let a= 3.14159;
+console.log(a.toFixed(2)); // Convert to Fixed Point Notation with 2 decimal places
+console.log(a.toFixed(4)); // Convert to Fixed Point Notation with 4 decimal places
+console.log(a.toFixed(6)); // Convert to Fixed Point Notation with 6 decimal
+
+//toPrecision() Method
+console.log(a.toPrecision(2)); // Convert to Precision with 2 significant digits   
+console.log(a.toPrecision(4)); // Convert to Precision with 4 significant digits
+console.log(a.toPrecision(6)); // Convert to Precision with 6 significant digits
+
+// Number Methods
+console.log("Number Methods:");
+console.log(Number(true)); // Convert Boolean to Number
+console.log(Number(false)); // Convert Boolean to Number
+console.log(Number("123")); // Convert String to Number
+console.log(Number("123abc")); // Invalid String to Number // NaN
+console.log(Number(" 10  ")); // Convert String with Spaces to Number // 10
+console.log(Number("")); // Convert Empty String to Number // 0
+
+console.log(Number(new Date("1970-01-01"))); // Convert Date to Number (Unix Epoch Time)
+console.log(Number("2023-10-01")); // Convert Date String to Number (NaN)
+ 
+// Parse Methods
+console.log("Parse Methods:"); 
+console.log(parseInt("120")); // Parse String to Integer
+console.log(parseInt("123abc")); // Parse String with Non-numeric Characters to Integer // 123
+console.log(parseInt("  -123  ")); // Parse String with Spaces to Integer // 123
+console.log(parseFloat("3.14")); // Parse String to Float
+console.log(parseFloat("3.14abc")); // Parse String with Non-numeric Characters to Float // 3.14
+
+// isNaN() Method
+console.log("isNaN Method:");   
+console.log(isNaN("Hello")); // Check if String is NaN // true
+console.log(isNaN("123")); // Check if String is NaN // false
+
+// isFinite() Method
+console.log("isFinite Method:");    
+console.log(isFinite(123)); // Check if Number is Finite // true
+console.log(isFinite(Infinity)); // Check if Infinity is Finite // false
+console.log(isFinite(NaN)); // Check if NaN is Finite // false
+
+// isInteger() Method
+console.log("isInteger Method:");
+console.log(Number.isInteger(123)); // Check if Number is Integer // true
+console.log(Number.isInteger(123.45)); // Check if Float is Integer // false
+console.log(Number.isInteger("123")); // Check if String is Integer // false
+
+// isSafeInteger() Method
+// A safe integer is an integer that can be exactly represented as a double precision number.
+// Safe integers are all integers from -(2^53 - 1) to +(2^53 - 1).
+console.log("isSafeInteger Method:");
+console.log(Number.isSafeInteger(123)); // Check if Number is Safe Integer // true
+console.log(Number.isSafeInteger(9007199254740991)); // Check if Large Number is Safe Integer // true
+console.log(Number.isSafeInteger(9007199254740992)); // Check if Larger Number is Safe Integer // false
+
+
+
