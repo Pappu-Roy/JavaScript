78669888123428d8c545e5a48c89033499029532
Pappu <pappuroy10102000@gmail.com>
2025-07-27 13:10:09 +0600

    JS DOM HTML

---
+++ DOM/03-DOM HTML/main.js
@@ -0,0 +1,8 @@
+// Changing Image in HTML document by id
+document.getElementById("img").src="https://c7.alamy.com/comp/DFG191/bangladesh-map-DFG191.jpg";
+
+const p = document.getElementById("p1");
+p.innerHTML="Hello Bangladesh!";
+
+// document.write() which is deprecated
+document.write(Date());
